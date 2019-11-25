fcbe0bd58fc5b9acb68517e2df25e3db3cb91276
wangfulong <wangfulong@yiqixie.com>
2019-11-25 18:04:00 +0800

    add static html

---
+++ test.js
@@ -0,0 +1,6 @@
+192.172.0.1 host1.class.net  host1
+192.172.0.1 host2.class.net  host2
+192.172.0.1 host3.class.net  host3
+192.172.0.1 host4.class.net  host4
+192.172.0.1 host5.class.net  host5
+192.172.0.1 host6.class.net  host6
