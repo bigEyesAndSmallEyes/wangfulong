c1d1987a5afc5d9263356871760cf017324a39a0
王复隆 <wangfulong@yiqixie.com>
2020-01-09 11:28:40 +0800

    update

--- rqstGraphql.sh
+++ rqstGraphql.sh
@@ -1,7 +1,9 @@
-curl -X POST \
+curl -i 'http://bjfk-staging-ls46.yz02:8090/graphql' \
+-H 'Accept-Encoding: gzip, deflate, br' \
+-H 'Connection: keep-alive' \
 -H "Content-Type: application/json" \
 -H "authorization: b-994_b8coNUYRGjlzUQ2Ta9Yr1QzsK58M7OpPsGWb3" \
--d '{"query": "{getUsersByUsername(username: "dongyong") {id}}"}' \
-http://bjfk-staging-ls46.yz02:8090/graphql
+-H 'Accept: application/json' \
+-d '{"query":"query {\n  getUsersByUsername(username: \"dongyong\"){\n    id\n  }\n}"}'
 
 
